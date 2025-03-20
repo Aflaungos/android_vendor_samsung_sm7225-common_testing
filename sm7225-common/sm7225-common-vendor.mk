50ec9b34fdae4b91d16839180c95c74043129a93
Aflaungos <vimavick@gmail.com>
2025-03-20 10:22:08 -0300

    Remove c2 manifest

--- sm7225-common/sm7225-common-vendor.mk
+++ sm7225-common/sm7225-common-vendor.mk
@@ -758,7 +758,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     com.qualcomm.qti.gpudrivers.lito.api30 \
     android.hardware.gnss@2.1-service-qti.xml \
-    c2_manifest_vendor.xml \
     android.hardware.neuralnetworks@1.3-service-qti-hta.xml \
     android.hardware.neuralnetworks@1.3-service-qti.xml \
     manifest_android.hardware.drm@1.3-service.widevine.xml \
